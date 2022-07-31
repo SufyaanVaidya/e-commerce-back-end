4911f854994c0a08a71e4061fa244693f8ccaa26
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-31 19:09:59 -0400

    defined categories

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    categories: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      autoIncrement: true
+    }
   },
   {
     sequelize,
