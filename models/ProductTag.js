9f547ea9562bca9f2d543e481ec7394ff20eca2d
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-31 19:48:26 -0400

    defined the product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { Product, Tag } = require('./Category');
 
 const sequelize = require('../config/connection');
 
@@ -6,8 +7,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+      
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'pag',
+        key: 'id'
+      }
+    }
+  },  
   {
     sequelize,
     timestamps: false,
