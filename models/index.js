5dde2205e8ca9f75955825dfbe6bbb2966bc62cc
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-31 20:00:05 -0400

    changed from to through on models index

--- models/index.js
+++ models/index.js
@@ -10,12 +10,12 @@ Product.belongsTo(Category, { foreignKey: 'category_id'});
 Category.hasMany(Product, { foreignKey: 'category_id', onDelete: 'CASCADE'});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  from: ProductTag,
+  through: ProductTag,
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  from: ProductTag,
+  through: ProductTag,
   foreignKey: 'tag_id'
 });
 module.exports = {
